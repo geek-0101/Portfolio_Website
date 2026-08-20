c828fbbcd8380aed3b809b19831d8749e229560e
Kopiko <paulinejoseph9a52gmail.com>
2026-08-20 22:15:38 +0530

    Day10_Changes

--- script.js
+++ script.js
@@ -60,3 +60,48 @@ window.addEventListener('scroll', () => {
         }
     })
 })
+
+//Day 10 - Project Filter System
+const projects =[
+    { id: 1, name: "Weather App", category: "web", tech: ["React", "API"] },
+    { id: 2, name: "Todo App", category: "web", tech: ["JavaScript"] },
+    { id: 3, name: "Portfolio", category: "design", tech: ["HTML", "CSS"] },
+    { id: 4, name: "Calculator", category: "web", tech: ["JavaScript"] },
+ 
+];
+ 
+function renderProjects(filter="all") {
+    const grid = document.querySelector('.projects-grid');
+    const filteredProjects = (filter === 'all' ? projects : projects.filter(p => p.category === filter));
+    
+    // HTML for project cards
+    grid.innerHTML = filteredProjects.map(project =>
+        `<article class="project-card">
+            <div class="project-card-body">
+                <h3>${project.name}</h3>
+                <div class="project-tags">
+                    ${project.tech.map(t => `<span class="tag">${t}</span>`).join(' | ')}      
+                </div>
+                <a href="#" class=" btn btn-primary">View Project</a>
+            </div>
+        </article>
+        `).join(''); 
+}
+ 
+//Filter buttons
+document.querySelectorAll('.filter-btn').forEach(btn =>{
+    btn.addEventListener('click',()=>{
+        //when a filter is clicked, it clears the active class from all 
+        //filter buttons and then adds it to the current filter
+        document.querySelectorAll('.filter-btn').forEach(b =>
+            b.classList.remove('active'));
+            btn.classList.add('active');
+            //when element with "data-*" attribute is cllicked, js removes the prefix
+            //and adds the rest of the attribute name into property on dataset object
+            renderProjects(btn.dataset.filter);
+    });
+});
+ 
+//Intial render
+renderProjects();
+ 
